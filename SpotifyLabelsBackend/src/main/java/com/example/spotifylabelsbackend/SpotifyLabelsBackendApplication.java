0e91e9b8b4209e4ccca5cf3cd5260357297e9175
DeszczMikolaj <deszcz.mikolaj@gmail.com>
2023-05-07 16:16:05 +0200

    backend init

---
+++ SpotifyLabelsBackend/src/main/java/com/example/spotifylabelsbackend/SpotifyLabelsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifylabelsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyLabelsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyLabelsBackendApplication.class, args);
+	}
+
+}
