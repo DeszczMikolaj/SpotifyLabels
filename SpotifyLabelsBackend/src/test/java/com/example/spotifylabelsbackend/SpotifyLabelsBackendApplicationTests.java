f662b24cb08ba13fcff07918a0515d96ad3c7fc0
DeszczMikolaj <deszcz.mikolaj@gmail.com>
2023-05-07 16:17:13 +0200

    backend init

---
+++ SpotifyLabelsBackend/src/test/java/com/example/spotifylabelsbackend/SpotifyLabelsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifylabelsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyLabelsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
